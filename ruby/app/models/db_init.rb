ad11704d9e9dc69c6512815f660cdd63b1d02233
Dmitry Kulikov <dima@koulikoff.ru>
2023-03-31 20:17:41 +0200

    ENV variable DB_PORT is added as I can not use the same port for PgSQL as on my machine

--- ruby/app/models/db_init.rb
+++ ruby/app/models/db_init.rb
@@ -1,11 +1,12 @@
 require 'active_record'
 
 DB = ActiveRecord::Base.establish_connection(
-  :adapter => 'postgresql',
-  :database => ENV['DB_NAME'],
-  :host => ENV['DB_HOST'],
-  :username => ENV['DB_USER'],
-  :password => ENV['DB_PASS'],
+  adapter: 'postgresql',
+  database: ENV['DB_NAME'],
+  host: ENV['DB_HOST'],
+  port: ENV['DB_PORT'],
+  username: ENV['DB_USER'],
+  password: ENV['DB_PASS'],
 )
 
 ActiveRecord::Base.connection.create_table(:articles, primary_key: 'id', force: true) do |t|
@@ -16,9 +17,9 @@ end
 
 require_relative 'article'
 
-Article.create(:title => 'Title ABC', :content => 'Lorem Ipsum', :created_at => Time.now)
-Article.create(:title => 'Title ZFX', :content => 'Some Blog Post', :created_at => Time.now)
-Article.create(:title => 'Title YNN', :content => 'O_O_Y_O_O', :created_at => Time.now)
+Article.create(title: 'Title ABC', content: 'Lorem Ipsum', created_at: Time.now)
+Article.create(title: 'Title ZFX', content: 'Some Blog Post', created_at: Time.now)
+Article.create(title: 'Title YNN', content: 'O_O_Y_O_O', created_at: Time.now)
 
 puts "Article count in DB: #{Article.count}"
 
